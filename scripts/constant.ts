fe43d4c9809036fb07b95374bafcce30443f42cb
Dunn <wg0121@outlook.com>
2026-01-23 14:06:32 +0800

    refactor(server): refactor project structure

--- scripts/constant.ts
+++ scripts/constant.ts
@@ -3,4 +3,4 @@ import path from "node:path";
 export const ROOT_DIR = path.resolve(import.meta.dirname, "..");
 export const DIST_DIR = path.resolve(ROOT_DIR, "./lib");
 export const SERVER_ENTRY_NAME = "main";
-export const SERVER_ENTRY_PATH = path.resolve(ROOT_DIR, `./src/${SERVER_ENTRY_NAME}.ts`);
+export const SERVER_ENTRY_PATH = path.resolve(ROOT_DIR, `./src/server/${SERVER_ENTRY_NAME}.ts`);
